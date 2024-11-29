f54476e5b2619ecd627c14b79267415840a6c2ac
thara.poludom@hotmail.com <thara.poludom@hotmail.com>
2024-11-30 01:31:42 +0700

    fix nextauth

--- src/app/api/auth/[...nextauth]/route.js
+++
@@ -1,7 +0,0 @@
-import NextAuth from "next-auth";
-import authOptions from '../../../lib/nextAuthOptions'
-
-// ใช้งาน NextAuth ใน API route
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST }; // รองรับทั้ง GET และ POST
